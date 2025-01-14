0068f59230e0fea56df4bf3ce683646f1c5b1d02
Zaki Omer <90000390+ZakiOmer22@users.noreply.github.com>
2025-01-14 19:19:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Lap_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.tmSidebar = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pnlSideBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.BackColor = System.Drawing.Color.Blue;
+            this.pnlSideBar.Controls.Add(this.button5);
+            this.pnlSideBar.Controls.Add(this.button1);
+            this.pnlSideBar.Controls.Add(this.button4);
+            this.pnlSideBar.Controls.Add(this.button2);
+            this.pnlSideBar.Controls.Add(this.button3);
+            this.pnlSideBar.Location = new System.Drawing.Point(282, 90);
+            this.pnlSideBar.MaximumSize = new System.Drawing.Size(156, 261);
+            this.pnlSideBar.MinimumSize = new System.Drawing.Size(156, 55);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(156, 55);
+            this.pnlSideBar.TabIndex = 1;
+            // 
+            // tmSidebar
+            // 
+            this.tmSidebar.Enabled = true;
+            this.tmSidebar.Interval = 10;
+            this.tmSidebar.Tick += new System.EventHandler(this.tmSidebar_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open Sidebar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-1, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Button 1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(-1, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Button 2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-1, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 55);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Button 3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(-1, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 55);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Button 4";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 456);
+            this.Controls.Add(this.pnlSideBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlSideBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Timer tmSidebar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// IMPORTING 
+using System.Data.SqlClient;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Lap_6
+{
+    public partial class Form1 : Form
+    {
+        bool menuIsCollapsed;
+        public Form1()
+        {
+            InitializeComponent();
+            //ConnectToDB();
+        }
+
+
+
+
+
+
+        //DB CONNECTION
+        private void ConnectToDB()
+        {
+            //SQL
+            try
+            {
+                //SQL CONNECTION
+                //1. Direct
+                String SqlConnectionString = "Data Source=ZAKI_OMER_22\\SQLEXPRESS;Initial Catalog=Lps;Integrated Security=True;";
+                SqlConnection con = new SqlConnection(SqlConnectionString);
+                con.Open();
+                con.Close();
+                //2. APP CONFIGURATION
+                String AppConfigSqlConnectionString = ConfigurationManager.ConnectionStrings["Lap_6.Properties.Settings.LpsConnectionString"].ConnectionString;
+                SqlConnection app_con = new SqlConnection(AppConfigSqlConnectionString);
+                app_con.Open();
+                app_con.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show($"Error is Occured: {error}");
+            }
+
+            //APP SERVER
+            try
+            {
+                //APP SERVER
+                //1. Direct Connection
+                String MySqlConnectionString = "Server=localhost;Database=UohC;User ID=root";
+                MySqlConnection MySqlCon = new MySqlConnection(MySqlConnectionString);
+                MySqlCon.Open();
+                MySqlCon.Close();
+                //2. App Config
+                string MySqlConnectionStringAppConfig = ConfigurationManager.ConnectionStrings["Lap_6.Properties.Settings.MySqlLpsConnectionString"].ConnectionString;
+                MySqlConnection connection = new MySqlConnection(MySqlConnectionStringAppConfig);
+                connection.Open();
+            }
+            catch (MySqlException error)
+            {
+                MessageBox.Show($"Error is Occured: {error}");
+            }
+        }
+
+        private void tmSidebar_Tick(object sender, EventArgs e)
+        {
+            if (menuIsCollapsed)
+            {
+                pnlSideBar.Height += 10;
+                if (pnlSideBar.Height == pnlSideBar.MaximumSize.Height)
+                {
+                    menuIsCollapsed = false;
+                    tmSidebar.Stop();
+                }
+            }
+            else
+            {
+                pnlSideBar.Height -= 10;
+                if (pnlSideBar.Height == pnlSideBar.MinimumSize.Height)
+                {
+                    menuIsCollapsed = true;
+                    tmSidebar.Stop();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tmSidebar.Start();
+        }
+    }
+}
+
+
+///SUMMARY
+///STEP 1 - CONNECTION STRING
+///          SERVERNAME COMPUTER_NAME/SQLEXPRESS  LOCALHOST ( 127.0.0.0 )
+///          DATABASENAME
+///          ACCESS TYPE  WINDOWS AUTH, USER, PASSWORD .....
+///STEP 2 - Type Of Connection
+///         DIRECTLY
+///         CONFIGURATION [ CONFIGURATION MANAGER /  App.config / ENV )
+///STEP 3 - DATABASE TYPE
+///         MYSQL
+///         SQL
+///         MongoDB
+///         FireBase
+///SQL CONNECTION STRING : Data Source=ZAKI_OMER_22\SQLEXPRESS;Initial Catalog=Lps;Integrated Security=True;
